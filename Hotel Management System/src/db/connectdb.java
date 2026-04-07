c95eb5f5557f6e8dd4d56b55f51ba1aee495bcbe
nguyenhoangbao21 <nguyenhoangbao2112@gmail.com>
2026-04-07 19:41:50 +0700

    Add files via upload

---
+++ Hotel Management System/src/db/connectdb.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package db;
+import java.sql.*;
+/**
+ *
+ * @author WOW
+ */
+public class connectdb {
+    public static Connection getCon(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/HMS?useSSL=false","root","Shiku270305");
+            return con;
+        }
+            catch (Exception e){
+                    return null;
+                    }
+    }
+}
+  
+
